1b1ed29c78ecee413264c60be804ee7d655ee643
Ethan Macalaguim <63220110+ItsThatMac@users.noreply.github.com>
2022-02-13 22:37:42 -0700

    Update attemptAtMonitor.cpp

--- Assignment 2/attemptAtMonitor.cpp
+++ Assignment 2/attemptAtMonitor.cpp
@@ -1,3 +1,6 @@
+// monitor.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
 #include <condition_variable> // std::condition_variale
 #include <cstdlib>
 #include <iostream>
@@ -10,48 +13,70 @@ std::condition_variable conditionalVariable;
 
 bool ready = false;
 int theData = 0;
+int count = 0;
 
 int produceData() {
-    int randomNumber = rand() % 1000;
-    std::cout << "produce data: " << randomNumber << "\n";
+    int randomNumber = rand() % 100 + 1;
+    std::cout << "///////////////////////////" << "\n" << "produce data: " << randomNumber << "\n" << "\n";
     return randomNumber;
 }
 
-void consumeData(int data) { std::cout << "receive data: " << data << "\n"; }
+void consumeData(int data) { std::cout << "///////////////////////////" << "\n" << "receive data: " << data << "\n" << "\n"; }
 
 void consumer() {
     int data = 0;
-    while (true) {
-        std::unique_lock<std::mutex> ul(theMutex);
-        conditionalVariable.wait(ul, []() { return ready; });
-        consumeData(theData);
-        ready = false;
-        ul.unlock();
-        conditionalVariable.notify_one();
+    int count = 0;
+    while (count < 10)
+    {
+        while (true) {
+            std::unique_lock<std::mutex> ul(theMutex);
+            // the monitor tht takes the input and determines whether or not to run the code.
+            conditionalVariable.wait(ul, []() { return ready; });
+            consumeData(rand() % theData + 1);
+            ready = false;
+            // the monitor then unlocks and allows the next process to continue
+            ul.unlock();
+            conditionalVariable.notify_one();
+            // ignore the count, it does nothing.
+            count++;
+        }
     }
 }
 
 void producer() {
-    while (true) {
-        std::unique_lock<std::mutex> ul(theMutex);
-        theData = produceData();
-        ready = true;
-        ul.unlock();
-        conditionalVariable.notify_one();
-        ul.lock();
-        conditionalVariable.wait(ul, []() { return ready == false; });
+    int count = 0;
+    while (count < 10)
+    {
+        while (true) {
+            // a mutex lock is placed on the process based on what the monitor sees
+            std::unique_lock<std::mutex> ul(theMutex);
+            theData = produceData();
+            ready = true;
+            // it will unlock the lock to complete the process and notify the next process
+            ul.unlock();
+            conditionalVariable.notify_one();
+            
+            ul.lock();
+            conditionalVariable.wait(ul, []() { return ready == false; });
+            count++;
+        }
     }
 }
 
+// Although I am using threads, it does not involve the pthread lock commands. 
+// It is simply to create new threads to use my monitor lock on.
 void consumerThread(int n) { consumer(); }
 
 void producerThread(int n) { producer(); }
 
 int main() {
-    int times = 100;
+    int times = 1;
     std::thread t1(consumerThread, times);
     std::thread t2(producerThread, times);
     t1.join();
     t2.join();
     return 0;
 }
+
+
+
