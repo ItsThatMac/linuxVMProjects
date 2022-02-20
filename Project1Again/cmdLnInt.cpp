981dea431634acc5442b1fd3bf4f308e5908cdd1
Kayla Zantello <59740873+kaylazantello@users.noreply.github.com>
2022-02-20 16:37:50 -0700

    code clean up & added comments

--- Project1Again/cmdLnInt.cpp
+++ Project1Again/cmdLnInt.cpp
@@ -38,6 +38,8 @@ int main() {
 
     string input;
     vector<vector<string>> commands{};
+    vector<string> temp{};
+    vector<string> cmd{};
 
     while(1) {
         
@@ -45,51 +47,52 @@ int main() {
         cout << "\n> ";
 
         commands.clear();  // clear command vector for new input
+        temp.clear();  // clear temp vector for new input
 
         getline(cin, input);  // get entire input entered by user (including spaces) and store in string variable called input
         size_t pos = 0;
         
-        vector<string> temp{};
+        // while a semicolon is present in string
         while((pos = input.find_first_of(';')) != string::npos) {
-            temp.push_back(input.substr(0, pos));
-            input.erase(0, pos + 1); 
+            temp.push_back(input.substr(0, pos));  // insert a substring of input from 0 to the semicolon into temp
+            input.erase(0, pos + 1);  // erase that same string, plus the semicolon, from input
         }
-        temp.push_back(input);
+        temp.push_back(input);  // insert the remaining input string into temp
 
-        vector<string> cmd{};
+        // for each string vector in temp
         for(int i = 0; i < temp.size(); i++) {
-            cmd.clear();
+            cmd.clear();  // clear the cmd vector for new input
+            
+            // while a blank character is present in the string
             while((pos = temp[i].find_first_of(' ')) != string::npos) {
+                // erase any leading blank characters before a string
                 if(temp[i].substr(0, pos).size() == 0) { 
                     temp[i].erase(0, pos + 1);
                     continue; 
                 }
-                cmd.push_back(temp[i].substr(0, pos));
-                temp[i].erase(0, pos + 1);
+                cmd.push_back(temp[i].substr(0, pos));  // insert a substring of temp[i] from 0 to the blank character into cmd
+                temp[i].erase(0, pos + 1);  // erase that same string, plus the trailing blank character, from temp[i]
             }
+            // erase any trailing blank characters after a string
             if(temp[i].size() == 0) {
                 commands.push_back(cmd);
                 continue;
             }
-            cmd.push_back(temp[i]);
-            commands.push_back(cmd);
+            cmd.push_back(temp[i]);  // insert remaining string at temp[i] into cmd
+            commands.push_back(cmd);  // insert cmd into commands
         }
 
-        cout << "size of commands vector: " << commands.size() << endl;
-
-        // print commands vector (for debugging purposes)
+        // print commands vector & other details (for debugging purposes)
+        /*cout << "size of commands vector: " << commands.size() << endl;
         for(int i = 0; i < commands.size(); i++) {
             cout << "commands[" << i << "] (" << commands[i].size() << ") = ";
             for(int j = 0; j < commands[i].size(); j++) {
                 cout << "|" << commands[i][j] << "|" << " (" << commands[i][j].size() << ")" << " ";
             }
             cout << endl;
-        }
+        }*/
         
-        //break;
-
-        //int i = 0;  // used for iteration through commands vector
-        
+        // for each cmd vector in commands
         for(int i = 0; i < commands.size(); i++) {
 
             // exit command line interpreter program
@@ -100,27 +103,24 @@ int main() {
             // print path of current directory
             if(commands[i][0] == "pwd") {
                 cout << pwd() << endl;
-                //i += 1;
                 continue;
             }
 
             // display list of the current directory's contents
             if(commands[i][0] == "ls") {
                 system(commands[i][0].c_str());  // convert command from string to char array, then use system() to execute command
-                //i += 1;
                 continue;
             }
 
+            // print the current date
             if(commands[i][0] == "date") {
                 system(commands[i][0].c_str());
-                //i += 1;
                 continue;
             }
 
             // create a new emtpy directory within current directory, name of new directory is specified by user
             if(commands[i][0] == "mkdir") {
                 mkdir(commands[i][1].c_str(), 0777);  // convert user's desired directory name to char array
-                //i += 2;
                 continue;
             }
 
@@ -132,52 +132,46 @@ int main() {
 
                 // convert path from string to char array, if rmdir() returns 0, then removal was successful
                 if(rmdir(dirPath.c_str()) == 0) {
-                    //i += 2;
                     continue;
                 }
                 else {
                     cout << "rmdir: failed to remove \'" << commands[i][1] << "\'" << endl;  // notify user if unsuccessful
-                    //i += 2;
                     continue;
                 }
             }
 
+            // change the current working directory
             if(commands[i][0] == "cd") {
                 chdir(commands[i][1].c_str());
-                // i+=2;
                 continue;
             }
 
+            // concatenate and print files
             if(commands[i][0] == "cat") {
                 if(commands[i][1] == ">") {
                     string newCommand = commands[i][0] + " > " + commands[i][2];
                     system(newCommand.c_str());
-                    // i+=3;
                     continue;
                 }
                 else {
                     string newCommand2 = commands[i][0] + " " + commands[i][1];
                     system(newCommand2.c_str());
-                    // i+=2;
                     continue;
                 }
             }
 
+            // display storage usage
             if(commands[i][0] == "df") {
                 system(commands[i][0].c_str());
                 continue;
             }
 
+            // display RAM availability
             if(commands[i][0] == "free") {
                 system(commands[i][0].c_str());
                 continue;
             }
-
-            // ignore any spaces before or after a command/prompt again if no input is entered
-            //if(commands[i][j] == " " || "") {
-                //i += 1;
-                //continue;
-            //}
+            
         }
     
     }
